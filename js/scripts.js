d257031ca02d92a647ea3ededfe458eb6ffa49ca
Jarek Latek <jareklatek@gmail.com>
2017-06-14 22:39:20 +0200

    Working carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,49 @@
 $(function() {
-	var carouselList = $("#carousel .slider-list");
-	function changeSlide() {
-		carouselList.animate({'marginLeft':-800}, 500, moveFirstSlide);	
+	var carousel = $("#carousel");
+	var carouselList = carousel.find("ul.slider-list");
+	var carouselMenu = carousel.find("ul.indicators");
+	// dots adding by <li> elements witch are as many as the number of slides
+	carouselList.find("li").each(function() {
+		carouselMenu.append("<li></li>");
+	});
+console.log(carouselList);
+	var dotsOption = carouselMenu.find("li");
+	// adding class active
+	dotsOption.first().addClass("active");
+	// dots click function
+	dotsOption.click(function() {
+		if ( !$(this).hasClass("active") ) {
+			target = $(this).index();
+			slideChange(target);
+		}
+	});
+	// automatic slide change function
+	function autoChangeSlide() {
+		target = dotsOption.siblings(".active").index();
+		target == dotsOption.length -1 ? target = 0 : target += 1;
+		slideChange(target);	
 	}
-	function changeSlidePrev() {
-		carouselList.animate({'marginRight':-800}, 500, moveFirstSlide);
+	// slide moving
+	function slideChange(target) {
+		carouselList.stop().animate({'marginLeft': -800 * target});
+		dotsOption.removeClass("active").eq(target).addClass("active");
+		resetInterval();
 	}
-	setInterval(changeSlide, 5000);
-	function moveFirstSlide() {
-		var firstSlide = carouselList.find("li:first");
-		var lastSlide = carouselList.find("li:last");
-		lastSlide.after(firstSlide);
-		carouselList.css({marginLeft:0});
-		// indicators change with slide
-		$('.indicators li.active').removeClass('active').next().add('.indicators li:first').last().addClass('active');
+	// interval set and reset
+	var autoMove = setInterval(function(){autoChangeSlide();}, 5000);
+	function resetInterval() {
+		clearInterval(autoMove);
+		autoMove = setInterval(function(){autoChangeSlide();}, 5000);
 	}
 	// left and right button slide change
-	$("#prev-slide").click(function() {				// nie działa lewy przycisk i nie mogę wymyślić jak go ożywić!!!
-		$(changeSlidePrev).carouselList('prev');
+	carousel.find(".prev").click(function() {
+		target = dotsOption.siblings(".active").index();
+		target == 0 ? target = dotsOption.length - 1 : target -= 1;
+		slideChange(target);
 	});
-	$("#next-slide").click(function() {
-		$(changeSlide).carouselList('next');
+	carousel.find(".next").click(function() {
+		target = dotsOption.siblings(".active").index();
+		target == dotsOption.length - 1 ? target = 0 : target += 1;
+		slideChange(target);
 	});
 });
